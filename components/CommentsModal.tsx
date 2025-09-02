995ef9d6b38245ce11c4ee3f4806b0a1a76ffcdc
Libasse Mbaye <libasselayembayedev@gmail.com>
2025-09-02 11:16:49 +0100

    Test

--- components/CommentsModal.tsx
+++ components/CommentsModal.tsx
@@ -16,6 +16,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { X, Send, Heart } from 'lucide-react-native';
 import { CommentDTO, CommentRequestDTO } from '@/models/app.interfaces';
 import { apiService } from '@/services/api';
+import SkeletonLoading from 'expo-skeleton-loading'
 
 const { height } = Dimensions.get('window');
 
@@ -46,14 +47,15 @@ export default function CommentsModal({
   onLikeComment,
 }: CommentsModalProps) {
   const [newComment, setNewComment] = useState('');
+   const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef<TextInput>(null);
    const [commentsData, setCommentData] = useState<CommentDTO[]>([]);
 
   useEffect(() => {
     if (visible) {
-      setTimeout(() => {
-        inputRef.current?.focus();
-      }, 300);
+      // setTimeout(() => {
+      //   inputRef.current?.focus();
+      // }, 300);
     }
   }, [visible]);
 
@@ -66,18 +68,31 @@ export default function CommentsModal({
     }
   };
 
+  // const likeComment = (commentId: number) => {
+  //     setCommentData(prev =>
+  //       prev.map((comment: any) =>
+  //         comment.id === commentId
+  //           ? {
+  //               ...comment,
+  //               likedByCurrentUser: !comment.likedByCurrentUser
+  //             }
+  //           : comment
+  //       )
+  //     );
+  // };
+
   const likeComment = (commentId: number) => {
-      setCommentData(prev =>
-        prev.map((comment: any) =>
-          comment.id === commentId
-            ? {
-                ...comment,
-                likedByCurrentUser: !comment.likedByCurrentUser
-              }
-            : comment
-        )
-      );
-  };
+  setCommentData(prev =>
+    prev.map((comment: CommentDTO) =>
+      comment.id === commentId
+        ? {
+            ...comment,
+            likedByCurrentUser: !comment.likedByCurrentUser,
+          }
+        : comment
+    )
+  );
+};
 
   const formatTimestamp = (timestamp: string) => {
     const now = new Date();
@@ -112,7 +127,7 @@ export default function CommentsModal({
         onPress={
           () => {
              likeComment(item.id);
-             handleLikeComment(item.id.toString())
+             handleLikeComment(item.id.toString(), false)
           }
       }
       >
@@ -130,6 +145,44 @@ export default function CommentsModal({
     </View>
   );
 
+
+  const renderSkeletonLoader = () => (
+    <SkeletonLoading background={"#adadad"} highlight={"#ffffff"}>
+      <View style={{ flexDirection: "column", padding: 10 }}>
+        {[...Array(5)].map((_, index) => (
+          <View
+            key={index}
+            style={{
+              marginBottom: 20,
+              backgroundColor: "#e0e0e0",
+              borderRadius: 10,
+              overflow: "hidden",
+            }}
+          >
+  
+            {/* Ligne avatar + pseudo */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                padding: 10
+              }}
+            >
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: "#adadad",
+                  marginRight: 10,
+                }}
+              />
+            </View>
+          </View>
+        ))}
+      </View>
+    </SkeletonLoading>
+  );
    const loadCOmmentsByVideoId = async (videoId: string) => {
       try {
         const response = await apiService.getVideoComments(Number(videoId));
@@ -143,13 +196,15 @@ export default function CommentsModal({
       }
   };
 
-    const handleLikeComment = async (commentId: string) => {
+    const handleLikeComment = async (commentId: string, reloading: boolean = true) => {
       console.log("handleLikeComment ", commentId);
       try {
         const resp = await apiService.likeComment(Number(commentId));
         console.log("Response like commenrt ", resp)
         likeComment(Number(commentId));
-        await loadCOmmentsByVideoId(videoId);
+        if (reloading) {
+          await loadCOmmentsByVideoId(videoId);
+        }
       } catch (error) {
         console.error("Une erreur est survenue ", error)
         Alert.alert("Erreur ")
@@ -203,20 +258,25 @@ export default function CommentsModal({
             </TouchableOpacity>
           </LinearGradient>
 
-          <FlatList
-            data={commentsData}
-            renderItem={renderComment}
-            keyExtractor={(item) => item.id.toString()}
-            style={styles.commentsList}
-            showsVerticalScrollIndicator={true}
-            contentContainerStyle={styles.commentsContent}
-            ListEmptyComponent={
-              <View style={styles.emptyState}>
-                <Text style={styles.emptyText}>No comments yet</Text>
-                <Text style={styles.emptySubtext}>Be the first to comment!</Text>
-              </View>
-            }
-          />
+          {isLoading ? (
+  renderSkeletonLoader()
+) : (
+  <FlatList
+    data={commentsData}
+    renderItem={renderComment}
+    keyExtractor={(item) => item.id.toString()}
+    style={styles.commentsList}
+    showsVerticalScrollIndicator={true}
+    contentContainerStyle={styles.commentsContent}
+    ListEmptyComponent={
+      <View style={styles.emptyState}>
+        <Text style={styles.emptyText}>No comments yet</Text>
+        <Text style={styles.emptySubtext}>Be the first to comment!</Text>
+      </View>
+    }
+  />
+)}
+
 
           <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
